f34ce416d91ab6abd9926b9d41ffec2aed5571a3
hongtienhaolk2001 <hongtienhaolk2001@gmail.com>
2024-06-30 22:51:53 +0700

    Update Skill Cloud

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@
 * Author: BootstrapMade.com
 * License: https://bootstrapmade.com/license/
 */
-(function() {
+(function () {
   "use strict";
 
   /**
@@ -48,7 +48,7 @@
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
+  on('click', '.mobile-nav-toggle', function (e) {
     select('#navbar').classList.toggle('navbar-mobile')
     this.classList.toggle('bi-list')
     this.classList.toggle('bi-x')
@@ -57,7 +57,7 @@
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '#navbar .nav-link', function(e) {
+  on('click', '#navbar .nav-link', function (e) {
     let section = select(this.hash)
     if (section) {
       e.preventDefault()
@@ -90,7 +90,7 @@
 
       if (!header.classList.contains('header-top')) {
         header.classList.add('header-top')
-        setTimeout(function() {
+        setTimeout(function () {
           sections.forEach((item) => {
             item.classList.remove('section-show')
           })
@@ -129,7 +129,7 @@
           }
         })
 
-        setTimeout(function() {
+        setTimeout(function () {
           initial_nav.classList.add('section-show')
         }, 350);
 
@@ -146,7 +146,7 @@
     new Waypoint({
       element: skilsContent,
       offset: '80%',
-      handler: function(direction) {
+      handler: function (direction) {
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
           el.style.width = el.getAttribute('aria-valuenow') + '%'
@@ -197,9 +197,9 @@
 
       let portfolioFilters = select('#portfolio-flters li', true);
 
-      on('click', '#portfolio-flters li', function(e) {
+      on('click', '#portfolio-flters li', function (e) {
         e.preventDefault();
-        portfolioFilters.forEach(function(el) {
+        portfolioFilters.forEach(function (el) {
           el.classList.remove('filter-active');
         });
         this.classList.add('filter-active');
@@ -251,3 +251,213 @@
   new PureCounter();
 
 })()
+
+////////////////////  ICON Cloud  ////////////////
+
+var radius = 100;
+var dtr = Math.PI / 180;
+var d = 300;
+
+var mcList = [];
+var active = false;
+var lasta = 1;
+var lastb = 1;
+var distr = true;
+var tspeed = 1;
+var size = 250;
+
+var mouseX = 0;
+var mouseY = 0;
+
+var howElliptical = 1;
+
+var aA = null;
+var oDiv = null;
+
+window.onload = function () {
+  var i = 0;
+  var oTag = null;
+
+  oDiv = document.getElementById('IconCloud');
+
+  aA = oDiv.getElementsByTagName('a');  
+  // aA=document.getElementById('div2');
+  // aA=oDiv.getElementsByTagName('span');
+
+  for (i = 0; i < aA.length; i++) {
+    oTag = {};
+
+    oTag.offsetWidth = aA[i].offsetWidth;
+    oTag.offsetHeight = aA[i].offsetHeight;
+
+    mcList.push(oTag);
+  }
+
+  sineCosine(0, 0, 0);
+
+  positionAll();
+
+  oDiv.onmouseover = function () {
+    active = true;
+  };
+
+  oDiv.onmouseout = function () {
+    active = false;
+  };
+
+  oDiv.onmousemove = function (ev) {
+    var oEvent = window.event || ev;
+
+    mouseX = oEvent.clientX - (oDiv.offsetLeft + oDiv.offsetWidth / 2);
+    mouseY = oEvent.clientY - (oDiv.offsetTop + oDiv.offsetHeight / 2);
+
+    mouseX /= 5;
+    mouseY /= 5;
+  };
+
+  setInterval(update, 30);
+};
+
+function update() {
+  var a;
+  var b;
+
+  if (active) {
+    a = (-Math.min(Math.max(-mouseY, -size), size) / radius) * tspeed;
+    b = (Math.min(Math.max(-mouseX, -size), size) / radius) * tspeed;
+  } else {
+    a = lasta * 0.98;
+    b = lastb * 0.98;
+  }
+
+  lasta = a;
+  lastb = b;
+
+  if (Math.abs(a) <= 0.01 && Math.abs(b) <= 0.01) {
+    return;
+  }
+
+  var c = 0;
+  sineCosine(a, b, c);
+  for (var j = 0; j < mcList.length; j++) {
+    var rx1 = mcList[j].cx;
+    var ry1 = mcList[j].cy * ca + mcList[j].cz * (-sa);
+    var rz1 = mcList[j].cy * sa + mcList[j].cz * ca;
+
+    var rx2 = rx1 * cb + rz1 * sb;
+    var ry2 = ry1;
+    var rz2 = rx1 * (-sb) + rz1 * cb;
+
+    var rx3 = rx2 * cc + ry2 * (-sc);
+    var ry3 = rx2 * sc + ry2 * cc;
+    var rz3 = rz2;
+
+    mcList[j].cx = rx3;
+    mcList[j].cy = ry3;
+    mcList[j].cz = rz3;
+
+    per = d / (d + rz3);
+
+    mcList[j].x = (howElliptical * rx3 * per) - (howElliptical * 2);
+    mcList[j].y = ry3 * per;
+    mcList[j].scale = per;
+    mcList[j].alpha = per;
+
+    mcList[j].alpha = (mcList[j].alpha - 0.6) * (10 / 6);
+  }
+
+  doPosition();
+  depthSort();
+}
+
+function depthSort() {
+  var i = 0;
+  var aTmp = [];
+
+  for (i = 0; i < aA.length; i++) {
+    aTmp.push(aA[i]);
+  }
+
+  aTmp.sort(
+    function (vItem1, vItem2) {
+      if (vItem1.cz > vItem2.cz) {
+        return -1;
+      } else if (vItem1.cz < vItem2.cz) {
+        return 1;
+      } else {
+        return 0;
+      }
+    }
+  );
+
+  for (i = 0; i < aTmp.length; i++) {
+    aTmp[i].style.zIndex = i;
+  }
+}
+
+function positionAll() {
+  var phi = 0;
+  var theta = 0;
+  var max = mcList.length;
+  var i = 0;
+
+  var aTmp = [];
+  var oFragment = document.createDocumentFragment();
+
+  //Ëæ»úÅÅÐò
+  for (i = 0; i < aA.length; i++) {
+    aTmp.push(aA[i]);
+  }
+
+  aTmp.sort(
+    function () {
+      return Math.random() < 0.5 ? 1 : -1;
+    }
+  );
+
+  for (i = 0; i < aTmp.length; i++) {
+    oFragment.appendChild(aTmp[i]);
+  }
+
+  oDiv.appendChild(oFragment);
+
+  for (var i = 1; i < max + 1; i++) {
+    if (distr) {
+      phi = Math.acos(-1 + (2 * i - 1) / max);
+      theta = Math.sqrt(max * Math.PI) * phi;
+    } else {
+      phi = Math.random() * (Math.PI);
+      theta = Math.random() * (2 * Math.PI);
+    }
+    //×ø±ê±ä»»
+    mcList[i - 1].cx = radius * Math.cos(theta) * Math.sin(phi);
+    mcList[i - 1].cy = radius * Math.sin(theta) * Math.sin(phi);
+    mcList[i - 1].cz = radius * Math.cos(phi);
+
+    aA[i - 1].style.left = mcList[i - 1].cx + oDiv.offsetWidth / 2 - mcList[i - 1].offsetWidth / 2 + 'px';
+    aA[i - 1].style.top = mcList[i - 1].cy + oDiv.offsetHeight / 2 - mcList[i - 1].offsetHeight / 2 + 'px';
+  }
+}
+
+function doPosition() {
+  var l = oDiv.offsetWidth / 2;
+  var t = oDiv.offsetHeight / 2;
+  for (var i = 0; i < mcList.length; i++) {
+    aA[i].style.left = mcList[i].cx + l - mcList[i].offsetWidth / 2 + 'px';
+    aA[i].style.top = mcList[i].cy + t - mcList[i].offsetHeight / 2 + 'px';
+
+    aA[i].style.fontSize = Math.ceil(12 * mcList[i].scale / 2) + 8 + 'px';
+
+    aA[i].style.filter = "alpha(opacity=" + 100 * mcList[i].alpha + ")";
+    aA[i].style.opacity = mcList[i].alpha;
+  }
+}
+
+function sineCosine(a, b, c) {
+  sa = Math.sin(a * dtr);
+  ca = Math.cos(a * dtr);
+  sb = Math.sin(b * dtr);
+  cb = Math.cos(b * dtr);
+  sc = Math.sin(c * dtr);
+  cc = Math.cos(c * dtr);
+}
